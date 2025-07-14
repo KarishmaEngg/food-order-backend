9b2acb8838eef24dfa9725f5acbcb1b508e5010b
KarishmaEngg <karishmasingh58638@gmail.com>
2025-07-14 22:34:26 +0530

    Clean commit with .gitignore applied

--- README.md
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-"# React-vercel-deploy-food-ordering-app" 
-=======
-# food-ordering-app
->>>>>>> 41a65c0312ba6c16982b4fafd3a773117ab9ec8f
--- routes/productRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { getAllProducts, addProduct } from "../controllers/productController.js";
-
-const router = express.Router();
-
-router.get("/", getAllProducts);
-router.post("/", addProduct);
-
-export default router;
